e04c40a909d1288ace9c8de8520ade55e9d1cde1
iyagami <yagami19972024@hotmail.com>
2025-03-12 11:37:02 -0500

    feat:REQ00000-Sync

--- curl-back.java
+++ curl-back.java
@@ -1,5 +1,17 @@
-API PARAMETROS:
+apim.oficinadigital.internal.psdesa.com.pe
 
+
+curl --location 'https://apim-eu1-border-service-desa.azure-api.net/un-gestion-parametro-eps/soporte-ti/v1/parametros?maestro=1000,1001' \
+--header 'X-Correlation-id: 0d40e6a3-c1ef-4829-9c1d-90406f5d99e2' \
+--header 'usuarioAplicacion: jocampo' \
+--header 'nombreAplicacion: ODS' \
+--header 'procesoNegocio: consulta-parametro' \
+--header 'ocp-apim-subscription-key: 3d6034bf2b9d4c92977c4bab76483c13' \
+--header 'X-Request-Id: 0d40e6a3-c1ef-4829-9c1d-90406f5d99e2'
+
+
+
+API PARAMETROS:
 curl --location 'https://apim-eu1-border-service-desa.azure-api.net/un-gestion-parametro-eps/soporte-ti/v1/parametros?maestro=1000,1001' \
 --header 'X-Correlation-id: 0d40e6a3-c1ef-4829-9c1d-90406f5d99e2' \
 --header 'usuarioAplicacion: jocampo' \
