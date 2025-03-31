f230ba45319e95e4ff489a5f9b582c76d651d1e5
yagami <yagami19972024@hotmail.com>
2025-03-31 01:58:32 -0500

    Sync

--- clipBoard-Jhordan.java
+++ clipBoard-Jhordan.java
@@ -295,48 +295,42 @@ chmod +x /usr/local/bin/yq
 
 
 
+// ********************************************************************************************** TEST ********************************************************************************************** //
+// ---------------------------------------------------------- [FSU] ---------------------------------------------------------- //
+<10.81.5.5> to <10.81.19.73>      	[HTTPS]	[443]		                      VM (vme1tstagedso02) al AS (web-eu1-ofidig-test-01)
+<10.81.5.5>	to <10.81.19.72>      	[HTTPS]	[443]		                      VM (vme1desagedso02) al AS (web-eu1-ofidig-test-02)	
+<10.81.5.5>	to <10.81.19.68>      	[HTTPS]	[443]		                      VM (vme1desagedso02) al AS (web-eu1-ofidig-test-03)	
+<10.81.5.5>	to <10.81.19.69>      	[HTTPS]	[443]		                      VM (vme1desagedso02) al AS (web-eu1-ofidig-test-05)	
+<10.81.5.5>	to <10.81.19.71>      	[HTTPS]	[443]		                      VM (vme1desagedso02) al AS (web-eu1-ofidig-test-04)	
+<10.81.5.5>	to <10.81.19.70>      	[HTTPS]	[443]		                      VM (vme1desagedso02) al AS (web-eu1-ofidig-test-06)	
+<10.81.5.5>	to <10.81.19.132>      	[HTTPS]	[443]		                      VM (vme1desagedso02) al AS (kveu1ofidigtst01)	
+<10.81.5.5>	to <10.81.23.164>      	[HTTPS]	[443]		                      VM (vme1desagedso02) al AS (kveu1datostest-01)	
+
+<10.81.4.84> to	<10.81.19.73>	      [HTTPS]	[443]		                      VM Jump (vme1tstdsojum01) al AS (web-eu1-ofidig-test-01)
+<10.81.4.84> to	<10.81.19.72>	      [HTTPS]	[443]		                      VM Jump (vme1tstdsojum01) al AS (web-eu1-ofidig-test-02)	
+<10.81.4.84> to	<10.81.19.68>	      [HTTPS]	[443]		                      VM Jump (vme1tstdsojum01) al AS (web-eu1-ofidig-test-03)	
+<10.81.4.84> to	<10.81.19.71>	      [HTTPS]	[443]		                      VM Jump (vme1tstdsojum01) al AS (web-eu1-ofidig-test-04)	
+<10.81.4.84> to	<10.81.19.69>	      [HTTPS]	[443]		                      VM Jump (vme1tstdsojum01) al AS (web-eu1-ofidig-test-05)
+<10.81.4.84> to	<10.81.19.70>	      [HTTPS]	[443]		                      VM Jump (vme1tstdsojum01) al AS (web-eu1-ofidig-test-06)	
+
+<10.228.40.0/21> to	<10.81.19.36>	  [TCP]	  [1433]		                    AKS (aks-eu1-integra-canal-test) al SSQL1-ODS   (db-srv-eu1-ofidig-test-01)
+<10.228.40.0/21> to	<10.81.19.52>	  [TCP]	  [1433]		                    AKS (aks-eu1-integra-canal-test) al SSQL2-ODS   (db-srv-eu1-ofidig-test-02)	
+<10.228.40.0/21> to	<10.81.23.132>	[TCP]	  [1433]		                    AKS (aks-eu1-integra-canal-test) al SSQL1-DATOS (db-srv-eu1-datos-test-01)
+<10.228.40.0/21> to	<10.81.19.4>	  [HTTPS]	[443]		                      AKS (aks-eu1-integra-canal-test) al SA1-ODS     (steu1ofidigapltest01)
+<10.228.40.0/21> to	<10.81.19.20>	  [HTTPS]	[443]		                      AKS (aks-eu1-integra-canal-test) al SA2-ODS     (steu1ofidigapltest02)
+<10.228.40.0/21> to	<10.81.19.228>	[HTTPS]	[443]		                      AKS (aks-eu1-integra-canal-test) al SA3-ODS     (steu1ofidigsrvltest01)
+<10.228.40.0/21> to	<10.81.19.196>	[HTTPS]	[443]		                      AKS (aks-eu1-integra-canal-test) al FN          (func-eu1-ofidig-test-01)
+<10.228.40.0/21> to	<10.81.19.132>	[HTTPS]	[443]		                      AKS (aks-eu1-integra-canal-test) al KV          (kveu1ofidigtst01)
+<10.228.48.0/21> to	<10.81.23.132>	[HTTPS]	[433]		                      AKS (aks-eu1-integra-multicanal-test) al SSQL1-DATOS (db-srv-eu1-datos-test-01)
+<10.228.48.0/21> to	<10.81.23.164>	[HTTPS]	[443]		                      AKS (aks-eu1-integra-multicanal-test) al KV          (kveu1datostest-01)
+
+
+<10.81.5.6>	to <10.81.19.36>	      [TCP]	  [1433]		                    VM (vme1tstagedso03) al SSQL1-ODS   (db-srv-eu1-ofidig-test-01)
+<10.81.5.6>	to <10.81.19.52>	      [TCP]	  [1433]		                    VM (vme1tstagedso03) al SSQL2-ODS   (db-srv-eu1-ofidig-test-02)
+<10.81.5.6>	to <10.81.23.132>	      [TCP]	  [1433]		                    VM (vme1tstagedso03) al SSQL1-DATOS (db-srv-eu1-datos-test-01)
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-// TEST[VALIDATE]
-
-10.81.5.5	10.81.19.73	HTTPS	443		                      Comunicacion de la VM (vme1tstagedso02) al App Service web-eu1-ofidig-test-01	
-10.81.5.5	10.81.19.72	HTTPS	443		                      Comunicacion del VM (vme1desagedso02) al App Service web-eu1-ofidig-test-02	
-10.81.5.5	10.81.19.68	HTTPS	443		                      Comunicacion del VM (vme1desagedso02) al App Service web-eu1-ofidig-test-03	
-10.81.5.5	10.81.19.71	HTTPS	443		                      Comunicacion del VM (vme1desagedso02) al App Service web-eu1-ofidig-test-04	
-10.81.5.5	10.81.19.69	HTTPS	443		                      Comunicacion del VM (vme1desagedso02) al App Service web-eu1-ofidig-test-05	
-10.81.5.5	10.81.19.70	HTTPS	443		                      Comunicacion del VM (vme1desagedso02) al App Service web-eu1-ofidig-test-06	
-
-10.228.40.0/21	10.81.19.36	TCP	1433		                Comunicacion del AKS al servidor Azure SQL 01 de Oficina Digital.	
-10.228.40.0/21	10.81.19.52	TCP	1433		                Comunicacion del AKS al servidor Azure SQL 02 de Oficina Digital.	
-10.228.40.0/21	10.81.19.4	HTTPS	443		                Comunicacion del AKS al Storage Account de Oficina Digital.	
-10.228.40.0/21	10.81.23.132	TCP	1433		              Comunicacion del AKS al servidor Azure SQL Datos.	
-10.228.40.0/21	10.81.19.20	HTTPS	443		                Comunicacion del AKS al Storage Account de Oficina Digital.	
-10.228.40.0/21	10.81.19.228	HTTPS	443		              Comunicacion del AKS al Storage Account de Oficina Digital.	
-10.228.40.0/21	10.81.19.196	HTTPS	443		              Comunicacion del AKS al Azure Function.	
-
-10.81.5.6	10.81.19.36	TCP	1433		                      Comunicacion del VM (vme1tstagedso03) al servidor Azure SQL 01 de Oficina Digital.	
-10.81.5.6	10.81.19.52	TCP	1433		                      Comunicacion del VM (vme1tstagedso03) al servidor Azure SQL 02 de Oficina Digital.	
-10.81.5.6	10.81.23.132	TCP	1433		                    Comunicacion del VM (vme1tstagedso03) al servidor Azure SQL 01 Datos.	
-
-10.81.4.84	10.81.19.73	HTTPS	443		                    Comunicacion del VM Jump (vme1tstdsojum01) al App Service web-eu1-ofidig-test-01	
-10.81.4.84	10.81.19.72	HTTPS	443		                    Comunicacion del VM Jump (vme1tstdsojum01) al App Service web-eu1-ofidig-test-02	
-10.81.4.84	10.81.19.68	HTTPS	443		                    Comunicacion del VM Jump (vme1tstdsojum01) al App Service web-eu1-ofidig-test-03	
-10.81.4.84	10.81.19.71	HTTPS	443		                    Comunicacion del VM Jump (vme1tstdsojum01) al App Service web-eu1-ofidig-test-04	
-10.81.4.84	10.81.19.69	HTTPS	443		                    Comunicacion del VM Jump (vme1tstdsojum01) al App Service web-eu1-ofidig-test-05	
-10.81.4.84	10.81.19.70	HTTPS	443		                    Comunicacion del VM Jump (vme1tstdsojum01) al App Service web-eu1-ofidig-test-06	
 
 // ************************************************* APP SERVICE ************************************************* //
   // --------------------------------- TEST --------------------------------- //
@@ -388,6 +382,11 @@ db-srv-eu1-datos-test-01.database.windows.net 10.81.23.132
   nslookup db-srv-eu1-datos-test-01.database.windows.net
   nc -zv db-srv-eu1-datos-test-01.database.windows.net 1433
 // ************************************************* STORAGE ACCOUNT ************************************************* //
+steu1ofidigapltest01.blob.core.windows.net    10.81.19.4
+steu1ofidigapltest02.blob.core.windows.net    10.81.19.20
+steu1ofidigsrvltest01.blob.core.windows.net   10.81.19.228
+
+// VALID
 nslookup steu1ofidigapltest01.blob.core.windows.net
 nc -zv steu1ofidigapltest01.blob.core.windows.net 443
 
@@ -400,3 +399,7 @@ nc -zv steu1ofidigsrvltest01.blob.core.windows.net 443
 // ************************************************* FUNCTION ************************************************* //
 nslookup func-eu1-ofidig-test-01.azurewebsites.net
 nc -zv  func-eu1-ofidig-test-01.azurewebsites.net 443
+
+// ************************************************* KV ************************************************* //
+kveu1ofidigtst01.vault.azure.net  10.81.19.132
+kveu1datostest-01.vault.azure.net 10.81.23.164
