6894f05e98fe808817cb2ecc87432ff236896176
yagami <yagami19972024@hotmail.com>
2025-05-08 10:55:58 -0500

    Sync

---
+++ z-request-sync.java
@@ -0,0 +1,7 @@
+curl --location 'https://aks.canal.oficinadigital.internal.psdesa.com.pe/ms-un-gestion-parametrosiniestros-eps/od/gestion-de-siniestros/v1/maestros?codMaestro=M0001' \
+--header 'X-Correlation-id: 0d40e6a3-c1ef-4829-9c1d-90406f5d99e2' \
+--header 'usuarioAplicacion: jocampo' \
+--header 'nombreAplicacion: ODS' \
+--header 'procesoNegocio: consulta-maestro' \
+--header 'ocp-apim-subscription-key: avc' \
+--header 'X-Request-Id: 0d40e6a3-c1ef-4829-9c1d-90406f5d99e2'
